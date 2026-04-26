40dd87a1fe74faf221f4d03a8d39650469a40e5c
Yash Sherekar <yashsherekar17@gmail.com>
2026-04-26 23:52:48 +0530

    Authentication bug resolved

--- foodies-api/src/main/java/com/foodDelivery/filters/JwtAuthenticationFilter.java
+++ foodies-api/src/main/java/com/foodDelivery/filters/JwtAuthenticationFilter.java
@@ -24,7 +24,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private JwtUtil jwtUtil;
     @Autowired
     private UserDetailsService userDetailsService;
-
+ 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
